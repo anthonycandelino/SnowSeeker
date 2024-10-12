f54aef7f6262317936ed91dc04dee490111d582d
Anthony Candelino <anthcandelino@gmail.com>
2024-10-11 21:14:00 -0400

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Anthony Candelino on 2024-10-11.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
